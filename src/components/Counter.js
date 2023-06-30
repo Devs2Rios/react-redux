e9fbb2e0175a254bfbc456cb4bf54f71e110811f
eduairet <hola@eduairet.com>
2023-06-29 23:09:56 -0600

    Increase Decrease

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  /*
+   * useSelector is a hook that allows you to access the state of the store.
+   * Behind the scenes it executes the function passed to it and returns the value of the state.
+   * It's automagically subscribed to the store, and updates the values reactively.
+   * When the element is unmounted, the subscription is automatically unsubscribed.
+   */
+  const counter = useSelector(state => state.counter),
+    dispatch = useDispatch(), // We need to call the hook to use it
+    incrementHandler = () => dispatch({ type: 'INCREASE' }),
+    decrementHandler = () => dispatch({ type: 'DECREASE' }),
+    toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div className={classes.actions}>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
